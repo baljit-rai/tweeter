6494add922bf53f2348a5988c5fdf7a4d9f78eff
Baljit Rai <bsrai91@gmail.com>
2017-09-21 16:47:58 +0000

    implement form toggle

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -9,50 +9,6 @@
 $(function() {
 
 
-  // var tweetData = [{
-  //   "user": {
-  //     "name": "Newton",
-  //     "avatars": {
-  //       "small": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-  //       "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-  //       "large": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-  //     },
-  //     "handle": "@SirIsaac"
-  //   },
-  //   "content": {
-  //     "text": "If I have seen further it is by standing on the shoulders of giants"
-  //   },
-  //   "created_at": 1461116232227
-  // }, {
-  //   "user": {
-  //     "name": "Descartes",
-  //     "avatars": {
-  //       "small": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-  //       "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-  //       "large": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-  //     },
-  //     "handle": "@rd"
-  //   },
-  //   "content": {
-  //     "text": "Je pense , donc je suis"
-  //   },
-  //   "created_at": 1461113959088
-  // }, {
-  //   "user": {
-  //     "name": "Johann von Goethe",
-  //     "avatars": {
-  //       "small": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-  //       "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-  //       "large": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-  //     },
-  //     "handle": "@johann49"
-  //   },
-  //   "content": {
-  //     "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-  //   },
-  //   "created_at": 1461113796368
-  // }];
-
   $("form").submit(function(event) { //serialize input into string obj NOT JSON
 
     event.preventDefault();
@@ -132,6 +88,19 @@ $(function() {
       }
     });
   }
+
+  //slide down toggle to add compose tweet container
+  $('.compose').on("click", function(event) {
+    event.preventDefault()
+    if ($(".new-tweet").is(":hidden")) {
+      $(".new-tweet").slideDown("slow");
+      //focus on that text area
+      $('#text-input').focus();
+    } else {
+      $(".new-tweet").slideUp("slow");
+    }
+  });
+
 
 
 
